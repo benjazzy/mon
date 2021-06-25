eb75d8ab22e8855a1f53cfaa3c2dda800e336b29
benjazzy <benjazzy64@gmail.com>
2021-06-25 17:25:45 -0400

    Fixed a thread safety issue

--- src/server/server.go
+++ src/server/server.go
@@ -6,11 +6,9 @@ import (
 	"fmt"
 	"github.com/gorilla/websocket"
 	"github.com/spf13/viper"
-	"html/template"
 	"log"
 	"mon/src/shared"
 	"net/http"
-	"strconv"
 	"strings"
 	"sync"
 	"time"
@@ -21,37 +19,10 @@ var addr = flag.String("addr", "localhost:8080", "http service address")
 var upgrader = websocket.Upgrader{} // use default options
 
 var clientSockets = make(map[string]*websocket.Conn)
+var socketsLock = &sync.Mutex{}
 
 var status = make(map[string]*shared.Status)
 var statusLock = &sync.Mutex{}
-
-var timers = make(map[string]*time.Timer)
-
-//func echo(w http.ResponseWriter, r *http.Request) {
-//	c, err := upgrader.Upgrade(w, r, nil)
-//	if err != nil {
-//		log.Print("upgrade:", err)
-//		return
-//	}
-//	defer c.Close()
-//	for {
-//		mt, message, err := c.ReadMessage()
-//		if err != nil {
-//			log.Println("read:", err)
-//			break
-//		}
-//		log.Printf("recv: %s", message)
-//		err = c.WriteMessage(mt, message)
-//		if err != nil {
-//			log.Println("write:", err)
-//			break
-//		}
-//	}
-//}
-//
-//func home(w http.ResponseWriter, r *http.Request) {
-//	homeTemplate.Execute(w, "ws://"+r.Host+"/echo")
-//}
 
 func receive(w http.ResponseWriter, r *http.Request) {
 	c, err := upgrader.Upgrade(w, r, nil)
@@ -68,7 +39,12 @@ func receive(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	socketsLock.Lock()
 	clientSockets[string(hostname)] = c
+	socketsLock.Unlock()
+
+	timer := time.NewTimer(time.Second * 5)
+	timerRunning := false
 
 	for {
 		_, message, err := c.ReadMessage()
@@ -96,40 +72,20 @@ func receive(w http.ResponseWriter, r *http.Request) {
 		}
 
 		statusLock.Lock()
-		if _, ok := timers[s.Hostname]; ok {
-			if !status[s.Hostname].Online {
-				timers[s.Hostname] = time.NewTimer(time.Second * 5)
-				go func() {
-					h := s.Hostname
-					//After the timer fires this code unblocks.
-					<-timers[h].C
-					statusLock.Lock()
-					status[h].Online = false
-					statusLock.Unlock()
-				}()
-			}
-		}
-		status[s.Hostname] = &s
-		statusLock.Unlock()
-
-		if _, ok := timers[s.Hostname]; ok {
-			timers[s.Hostname].Reset(time.Second * 5)
-		} else {
-			timers[s.Hostname] = time.NewTimer(time.Second * 5)
+		timer.Reset(time.Second * 5)
+		if !timerRunning {
+			timerRunning = true
 			go func() {
 				h := s.Hostname
 				//After the timer fires this code unblocks.
-				<-timers[h].C
+				<-timer.C
 				statusLock.Lock()
 				status[h].Online = false
 				statusLock.Unlock()
 			}()
 		}
-
-		//err = c.WriteMessage(mt, message)
-		//if err != nil {
-		//	log.Println("write:", err)
-		//}
+		status[s.Hostname] = &s
+		statusLock.Unlock()
 	}
 }
 
@@ -160,11 +116,10 @@ func send(w http.ResponseWriter, r *http.Request) {
 					clients[client] = &c
 				}
 
+				statusLock.Lock()
 				for hostname, client := range status {
 					clients[hostname] = client
 				}
-
-				statusLock.Lock()
 				m, err := json.Marshal(clients)
 				statusLock.Unlock()
 				if err != nil {
@@ -182,18 +137,27 @@ func send(w http.ResponseWriter, r *http.Request) {
 		} else if len(splitMessage) == 2 {
 			if splitMessage[0] == "reboot" {
 				log.Println("Restarting:", splitMessage[1])
-				if client, ok := status[splitMessage[1]]; ok {
+
+				statusLock.Lock()
+				client, ok := status[splitMessage[1]]
+				statusLock.Unlock()
+				if ok {
 					if client.Online {
+						socketsLock.Lock()
 						if conn, ok := clientSockets[splitMessage[1]]; ok {
 							err := conn.WriteMessage(websocket.TextMessage, []byte("reboot"))
 							if err != nil {
 								log.Println("Error sending restart command to:", splitMessage[1])
 							}
 						}
+						socketsLock.Unlock()
 					}
 				}
 			} else if splitMessage[0] == "add" {
-				if client, ok := status[splitMessage[1]]; ok {
+				statusLock.Lock()
+				client, ok := status[splitMessage[1]]
+				statusLock.Unlock()
+				if ok {
 					contains := false
 					for _, c := range viper.GetStringSlice("clients") {
 						if c == client.Hostname {
@@ -210,21 +174,6 @@ func send(w http.ResponseWriter, r *http.Request) {
 						}
 					} else {
 						log.Println("client ", client.Hostname, "already in config")
-					}
-				}
-			} else if splitMessage[0] == "check" {
-				if client, ok := status[splitMessage[1]]; ok {
-					contains := false
-					for _, c := range viper.GetStringSlice("clients") {
-						if c == client.Hostname {
-							contains = true
-							break
-						}
-					}
-					err := c.WriteMessage(websocket.TextMessage, []byte(client.Hostname+" "+strconv.FormatBool(contains)))
-					if err != nil {
-						log.Println("write:", err)
-						break
 					}
 				}
 			} else if splitMessage[0] == "remove" {
@@ -269,83 +218,7 @@ func main() {
 	upgrader.CheckOrigin = func(r *http.Request) bool {
 		return true
 	}
-	//http.HandleFunc("/echo", echo)
 	http.HandleFunc("/receive", receive)
 	http.HandleFunc("/send", send)
-	//http.HandleFunc("/", home)
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
-
-var homeTemplate = template.Must(template.New("").Parse(`
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8">
-<script>  
-window.addEventListener("load", function(evt) {
-    var output = document.getElementById("output");
-    var input = document.getElementById("input");
-    var ws;
-    var print = function(message) {
-        var d = document.createElement("div");
-        d.textContent = message;
-        output.appendChild(d);
-        output.scroll(0, output.scrollHeight);
-    };
-    document.getElementById("open").onclick = function(evt) {
-        if (ws) {
-            return false;
-        }
-        ws = new WebSocket("{{.}}");
-        ws.onopen = function(evt) {
-            print("OPEN");
-        }
-        ws.onclose = function(evt) {
-            print("CLOSE");
-            ws = null;
-        }
-        ws.onmessage = function(evt) {
-            print("RESPONSE: " + evt.data);
-        }
-        ws.onerror = function(evt) {
-            print("ERROR: " + evt.data);
-        }
-        return false;
-    };
-    document.getElementById("send").onclick = function(evt) {
-        if (!ws) {
-            return false;
-        }
-        print("SEND: " + input.value);
-        ws.send(input.value);
-        return false;
-    };
-    document.getElementById("close").onclick = function(evt) {
-        if (!ws) {
-            return false;
-        }
-        ws.close();
-        return false;
-    };
-});
-</script>
-</head>
-<body>
-<table>
-<tr><td valign="top" width="50%">
-<p>Click "Open" to create a connection to the server, 
-"Send" to send a message to the server and "Close" to close the connection. 
-You can change the message and send multiple times.
-<p>
-<form>
-<button id="open">Open</button>
-<button id="close">Close</button>
-<p><input id="input" type="text" value="Hello world!">
-<button id="send">Send</button>
-</form>
-</td><td valign="top" width="50%">
-<div id="output" style="max-height: 70vh;overflow-y: scroll;"></div>
-</td></tr></table>
-</body>
-</html>
-`))
